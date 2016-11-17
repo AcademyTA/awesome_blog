a820f719fb4e70beefbb0fdfd66736d154867854
Jason <cameljeet@gmail.com>
2016-11-16 21:27:34 -0800

    Add many to many relationship for Articles and Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, only: [:new, :create]
+  before_action :find_category, only: [:show, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -21,9 +22,26 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = 'Category name was successfully updated.'
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 
   def require_admin
     if !logged_in? || (logged_in? and !current_user.admin?)
